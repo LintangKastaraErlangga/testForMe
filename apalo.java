82739a8ddf3d4a2b52e37438deca6dbefdaf6481
Ringo-K <lintangkastarae@gmail.com>
2024-11-17 15:26:17 +0700

    added new message again

--- apalo.java
+++ apalo.java
@@ -4,5 +4,6 @@ public class apalo {
     public static void main(String[] args) {
         System.out.println("odading mang oleh");
         System.out.println("rasanya seperti menjadi iron men");
+        System.out.println("belilah odading mang oleh");
     }
 }
